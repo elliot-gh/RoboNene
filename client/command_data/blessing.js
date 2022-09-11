dc51f65dccabfcd583c0de096615fe35dd1b31b3
Ai0796 <mo.alden25@gmail.com>
2022-09-11 17:26:09 +0000

    added a /blessing

---
+++ client/command_data/blessing.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Command Data & Constants Related to the /rank command
+ * These constants are used to dynamically generate a slash command on discord.
+ * This File also contains the constants values used exclusively in the /rank command.
+ * @author Potor10
+ */
+
+module.exports = {
+    'INFO': {
+        'name': 'blessing',
+        'utilization': '/blessing',
+        'description': 'blessing',
+        'ephemeral': false,
+    },
+
+    'CONSTANTS': {
+        'NO_ACC_ERROR': {
+            'type': 'Error',
+            'message': 'This user has not linked their project sekai account with the bot.'
+        }
+    }
+}
