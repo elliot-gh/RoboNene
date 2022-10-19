82850d6aed2005a28507d0690a544f29802a6b75
Ai0796 <mo.alden25@gmail.com>
2022-10-19 19:22:16 +0000

    Added command data for /heatmap

---
+++ client/command_data/heatmap.js
@@ -0,0 +1,67 @@
+/**
+ * @fileoverview Command Data & Constants Related to the /heatmap command
+ * These constants are used to dynamically generate a slash command on discord.
+ * This File also contains the constants and error values used exclusively in the /heatmap command.
+ * @author Ai0796
+ */
+
+module.exports = {
+  'INFO': {
+    'name': 'heatmap',
+    'utilization': '/heatmap',
+    'description': 'Display a histogram displaying the historical amount of points for a user or tier',
+    'ephemeral': false,
+    'subcommands': [
+      {
+        'name': 'cutoff',
+        'description': 'Get histogram of a tier cutoff over time',
+        'params' : [
+          {
+            'type': 'integer',
+            'name': 'tier',
+            'required': true,
+            'description': 'The cutoff tier specified',
+          },
+          {
+            'type': 'integer',
+            'name': 'offset',
+            'required': false,
+            'description': 'Offset from UTC'
+          }
+        ]
+      },
+      {
+        'name': 'user',
+        'description': 'Get histogram of a user over time',
+        'params': [
+          {
+          'type': 'user',
+          'name': 'user',
+          'required': true,
+          'description': 'A linked User that has been tracked'
+          },
+          {
+            'type': 'integer',
+            'name': 'offset',
+            'required': false,
+            'description': 'Offset from UTC'
+          }
+        ]
+      }
+    ]
+  },
+
+  'CONSTANTS': {
+    'NO_EVENT_ERR': {
+      'type': 'Error',
+      'message': 'There is currently no event going on'
+    },
+  
+    'NO_DATA_ERR': {
+      'type': 'Error',
+      'message': 'No data found (have you played a game after linking?)'
+    },
+  
+    'SEKAI_BEST_HOST': 'api.sekai.best'
+  }
+}
