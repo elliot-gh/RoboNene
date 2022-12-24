91a67aec18d92d7513414825e3a9dd91fe281b8e
Ai0796 <mo.alden25@gmail.com>
2022-12-24 02:47:14 +0000

    Created initial stock market client

---
+++ client/stock/stock.js
@@ -0,0 +1,56 @@
+/**
+ * @fileoverview Stock API code. Made by Ai0796, why have i done this
+ */
+
+const https = require('https');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+class Stock {
+    constructor(APIKEY) {
+        this.APIKEY = APIKEY;
+    }
+
+    async formatKey(key) {
+        var i, frags = key.split(' ');
+        for (i = 0; i < frags.length; i++) {
+            frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+        }
+        return frags.join(' ');
+    }
+
+    /**
+     * @typedef {Object} StockData
+     * @property {String} symbol symbol of the stock ticker
+     * @property {String} open opening price of the stock
+     * @property {String} high highest price of the stock
+     * @property {String} low lowest price of the stock
+     * @property {String} price current price of the stock
+     * @property {String} volume volume of the stock
+     * @property {String} latestTradingDay latest trading day of the stock
+     * @property {String} previousClose previous close of the stock
+     * @property {String} change change of the stock
+     * @property {String} changePercent change percent of the stock
+     */
+
+    /**
+     * @param {String} symbol stock symbol, or optionally a project sekai character/ticker
+     * @returns {Promise<StockData>} containing stock data
+     */
+    async getStockData(symbol) {
+
+        let response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${this.APIKEY}`);
+        let data = await response.json();
+
+        let returnData = {};
+
+        for (let key in data['Global Quote']) {
+            let newKey = await this.formatKey(key.substring(4));
+            returnData[newKey] = data['Global Quote'][key];
+        
+        }
+
+        return returnData;
+    }
+}
+
+module.exports = Stock;
