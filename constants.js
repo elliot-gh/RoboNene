b02ea8610cb04da2889a306147099ae107883dab
Ai0796 <mo.alden25@gmail.com>
2024-03-12 16:06:42 +0000

    changed cutoff tracking interval back to 1 minute

--- constants.js
+++ constants.js
@@ -32,7 +32,7 @@ module.exports = {
   'RATE_LIMIT': 120,
 
   //Amount of Time between Cutoff data in milliseconds
-  'CUTOFF_INTERVAL': 90000,
+  'CUTOFF_INTERVAL': 60000,
 
   //Amount of Time between Twitter calls
   'TWITTER_INTERVAL': 120000,
