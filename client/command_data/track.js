3dd4c922bd0390c206cfdfe458cde4c009afc98d
Ai0796 <mo.alden25@gmail.com>
2024-03-23 19:30:54 +0000

    updated track to have a user game tracking

--- client/command_data/track.js
+++ client/command_data/track.js
@@ -11,20 +11,75 @@ module.exports = {
         'utilization': '/track',
         'description': 'Track a rank to see if it moves and ping the user if it does',
         'ephemeral': false,
-        'params': [
+        'subcommands': [
             {
-                'type': 'integer',
                 'name': 'tier',
-                'required': true,
-                'description': 'The tier specified'
+                'description': 'Track a tier to see if it moves and ping the user if it does',
+                'params': [
+                    {
+                        'type': 'integer',
+                        'name': 'tier',
+                        'required': true,
+                        'description': 'The tier specified on the leaderboard',
+                        'maxValue': 100
+                    },
+                    {
+                        'type': 'integer',
+                        'name': 'cutoff',
+                        'required': false,
+                        'description': 'Optional cutoff to track e.g. 2,500,000 will alert you when the tier moves past 2.5mil'
+                    }
+                ]
             },
             {
-                'type': 'integer',
-                'name': 'cutoff',
-                'required': false,
-                'description': 'Optional cutoff to track e.g. 2,500,000 will alert you when the tier moves past 2.5mil'
+                'name': 'user',
+                'description': 'Tracks a specific tiers user to see if they hit a specific EP (max 5 per server)',
+                'params': [
+                    {
+                        'type': 'integer',
+                        'name': 'tier',
+                        'required': true,
+                        'description': 'A tier on the leaderboard',
+                        'maxValue': 100
+                    },
+                    {
+                        'type': 'integer',
+                        'name': 'cutoff',
+                        'required': false,
+                        'description': 'Optional cutoff to track e.g. 2,500,000 will alert you when the user hits 2.5mil'
+                    },
+                    {
+                        'type': 'integer',
+                        'name': 'min',
+                        'required': false,
+                        'description': 'The minimum score to ping'
+                    },
+                    {
+                        'type': 'integer',
+                        'name': 'max',
+                        'required': false,
+                        'description': 'The maximum score to ping'
+                    }
+                ]
+            },
+            {
+                'name': 'list',
+                'description': 'List all the current tracked tiers/users (if Admin shows all in channel)',
+            },
+            {
+                'name': 'remove',
+                'description': 'Remove a tracked tier/user',
+                'params': [
+                    {
+                        'type': 'integer',
+                        'name': 'num',
+                        'required': false,
+                        'description': 'The tracker to remove'
+                    }
+                ]
             }
         ]
+            
     },
 
     'CONSTANTS': {
@@ -40,7 +95,7 @@ module.exports = {
 
         'TIER_ERR': {
             'type': 'Error',
-            'message': 'Please choose a cutoff Tier Less than or Equal to 100000'
+            'message': 'Please choose a cutoff Tier Less than or Equal to 100'
         },
 
         'SEKAI_BEST_HOST': 'api.sekai.best'
--- client/commands/track.js
+++ client/commands/track.js
@@ -12,6 +12,16 @@ const generateSlashCommand = require('../methods/generateSlashCommand');
 const generateEmbed = require('../methods/generateEmbed');
 
 const fp = './JSONs/track.json';
+const userFp = './JSONs/userTrack.json';
+
+function checkIsAdmin(msg) {
+    try {
+        return msg.member.permissionsIn(msg.channel).has('Administrator');
+    }
+    catch {
+        return false;
+    }
+}
 
 function addTrack(tier, score, mention, channel) {
     tier = tier.toString();
@@ -44,6 +54,198 @@ function addTrack(tier, score, mention, channel) {
         });
     } catch (e) {
         console.log('Error occured while writing Tracking: ', e);
+    }
+}
+
+function getUserTrackFile() {
+    try {
+        if (!fs.existsSync(userFp)) {
+            return [];
+        }
+        else {
+            return JSON.parse(fs.readFileSync(userFp, 'utf8'));
+        }
+    } catch (e) {
+        console.log('Error occured while reading user tracking: ', e);
+    }
+}
+
+function saveUserTrackFile(object) {
+    fs.writeFile(userFp, JSON.stringify(object), err => {
+        if (err) {
+            console.log('Error writing user tracking', err);
+        } else {
+            console.log('Wrote user tracking Successfully');
+        }
+    });
+}
+
+async function sendUserTrack(discordClient, interaction) {
+    try {
+        let serverid = interaction.guild.id;
+        let userTrackFile = getUserTrackFile()[serverid] || [];
+        
+        let isAdmin = checkIsAdmin(interaction.member);
+        let userId = interaction.member.user.id;
+        let message = '';
+        let count = 0;
+        userTrackFile.forEach((trackObject) => {
+            if (trackObject.serverid == serverid && (isAdmin || trackObject.userId == userId)) {
+                message += `${++count}\n${formatTrackMessage(trackObject)}\n`;
+            }
+        });
+        if (message === '') {
+            message = 'No trackings found';
+        }
+        await interaction.editReply({
+            embeds: [
+                generateEmbed({
+                    name: COMMAND.INFO.name,
+                    content: {
+                        type: 'Success',
+                        message: message
+                    },
+                    client: discordClient.client
+                })
+            ]
+        });
+    } catch (e) {
+        console.log('Error occured while sending user tracking: ', e);
+    }
+}
+
+function formatTrackMessage(trackObject) {
+    let settingsText = '';
+
+    settingsText += `Added tracking for ${trackObject.name} ${trackObject.currentTier} with cutoff ${trackObject.cutoff.toLocaleString()}\n`;
+
+    if (trackObject.min > 100) {
+        settingsText += `Min: ${trackObject.min.toLocaleString()}\n`;
+    }
+
+    if (trackObject.max < Number.MAX_SAFE_INTEGER) {
+        settingsText += `Max: ${trackObject.max.toLocaleString()}\n`;
+    }
+
+    return settingsText;
+}
+
+async function addUserTrack(discordClient, interaction) {
+    discordClient.addPrioritySekaiRequest('ranking', {}, async (response) => {
+        try {
+            let userTrackFile = getUserTrackFile();
+            let tier = interaction.options.getInteger('tier');
+
+            if(tier > 100) {
+                await interaction.editReply({
+                    embeds: [
+                        generateEmbed({
+                            name: COMMAND.INFO.name,
+                            content: COMMAND.CONSTANTS.TIER_ERR,
+                            client: discordClient.client
+                        })
+                    ]
+                });
+                return;
+            }
+            
+            let userId = interaction.member.user.id;
+            let serverid = interaction.guild.id;
+            let name = response['rankings'][tier-1]['name'];
+            let cutoff = interaction.options.getInteger('cutoff') || response['rankings'][tier - 1]['score'] + 1;
+            let min = interaction.options.getInteger('min') || 100;
+            let max = interaction.options.getInteger('max') || Number.MAX_SAFE_INTEGER;
+            let trackId = response['rankings'][tier - 1]['userId'];
+            userTrackFile = userTrackFile || [];
+            
+            let trackObject = {
+                userId: userId,
+                currentTier: tier,
+                cutoff: cutoff,
+                min: min,
+                max: max,
+                trackId: trackId,
+                channel: interaction.channelId,
+                lastScore: response['rankings'][tier - 1]['score'],
+                inLeaderboard: true,
+                name: name,
+                serverid: serverid
+            };
+
+            userTrackFile.push(trackObject);
+            let settingsText = formatTrackMessage(trackObject);
+
+            saveUserTrackFile(userTrackFile);
+            await interaction.editReply({
+                embeds: [
+                    generateEmbed({
+                        name: COMMAND.INFO.name,
+                        content: {
+                            type: 'Success',
+                            message: settingsText
+                        },
+                        client: discordClient.client
+                    })
+                ]
+            });
+        } catch (e) {
+            console.log('Error occured while adding user tracking: ', e);
+        }
+    });
+}
+
+async function removeUserTrack(discordClient, interaction) {
+    try {
+        let serverid = interaction.guild.id;
+        let userTrackFile = getUserTrackFile();
+        let num = interaction.options.getInteger('num');
+        let userId = interaction.member.user.id;
+        let isAdmin = checkIsAdmin(interaction.member);
+        let tracks = [];
+        let index = 0;
+        userTrackFile.forEach(trackObject => {
+            console.log(JSON.stringify(trackObject));
+            if (trackObject.serverid == serverid && (isAdmin || trackObject.userId == userId)) {
+                tracks.push(index);
+            }
+            index++;
+        });
+
+        if (num < 0 || num >= tracks.length) {
+            await interaction.editReply({
+                embeds: [
+                    generateEmbed({
+                        name: COMMAND.INFO.name,
+                        content: {
+                            type: 'Error',
+                            message: `Invalid tracker number. Please use a number between 1 and ${tracks.length}`
+                        },
+                        client: discordClient.client
+                    })
+                ]
+            });
+            return;
+        }
+
+        let track = tracks[num-1];
+        userTrackFile[serverid].splice(track - 1, 1);
+        saveUserTrackFile(userTrackFile);
+
+        await interaction.editReply({
+            embeds: [
+                generateEmbed({
+                    name: COMMAND.INFO.name,
+                    content: {
+                        type: 'Success',
+                        message: `Removed tracking for:\n${formatTrackMessage(track)}`
+                    },
+                    client: discordClient.client
+                })
+            ]
+        });
+
+    } catch (e) {
+        console.log('Error occured while removing user tracking: ', e);
     }
 }
 
@@ -86,6 +288,17 @@ module.exports = {
         await interaction.deferReply({
             ephemeral: COMMAND.INFO.ephemeral
         });
+
+        if (interaction.options.getSubcommand() === 'list') {
+            await sendUserTrack(discordClient, interaction);
+            return;
+        } else if (interaction.options.getSubcommand() === 'user') {
+            await addUserTrack(discordClient, interaction);
+            return;
+        } else if (interaction.options.getSubcommand() === 'remove') {
+            await removeUserTrack(discordClient, interaction);
+            return;
+        }
 
         const event = getRankingEvent();
         if (event.id === -1) {
